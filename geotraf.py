e3d0b51139ea8f7520aa482d2ba60962dcb828ed
Holger Kruse <mail2holger@gmail.com>
2016-01-14 17:17:05 +0100

    lots of stuff

--- geotraf.py
+++ geotraf.py
@@ -30,7 +30,6 @@ def writexmol(name,nat,XYZ):
   print >>ofile, 'modified by TRxyz'
   for i in range(0,nat):
     print >>ofile,  str("% 5.5s % 4.12f % 4.12f % 4.12f" % (elem[i], float(XYZ[i,0]), float(XYZ[i,1]), float(XYZ[i,2]) ))
-#    ofile.write(str(XYZ[i]))
   ofile.close()
 
 
@@ -104,23 +103,35 @@ def rotvec(axis,vec,degree):
 def rotmol(at1,at2,degree,atlist,XYZ):
     p1=XYZ[at1,:]
     p2=XYZ[at2,:]
-    axis=np.subtract(p1,p2)
+    axis=np.subtract(p2,p1)
+    rad=radians(degree)
     for i in atlist[:]:
-      XYZ[i]=rotvec(axis,XYZ[i,:],degree)
+     v=XYZ[i,:]
+     dum= RotVecArb(axis,rad,p2,v)
+     Rmat= RotMatArb(axis,rad,p2,v)
+     vec=np.ones(4)
+     vec[:3]=v[:3]
+     print vec
+     print np.dot(vec,Rmat)
+#     print rotvec(axis,v,degree)
+#     XYZ[i,:]=RotVecArb(axis,rad,p2,v)
+#     XYZ[i,:]=RotMatArb(axis,rad,p2,v)
+#      v=np.subtract(XYZ[i,:],p2[:])
+#      XYZ[i,:]=rotvec(axis,v,degree)+p2
+#      XYZ[i]=rotvec(axis,v,degree)
     return 
 
 def rotmolMAT(at1,at2,degree,atlist,XYZ):
     p1=XYZ[at1,:]
     p2=XYZ[at2,:]
     axis=np.subtract(p2,p1)
-    k=normalize2(axis)
+    axis=np.subtract(axis,p2)
     rad=radians(degree)
     for i in atlist[:]:
-      v=XYZ[i,:]
-#      kxv=np.cross(k,v)
-#      kv=np.inner(k,v)
-#      XYZ[i,:]= v[:]*cos(rad) + kxv[:]*sin(rad) + k[:]*kv*(1.0 - cos(rad))
-      XYZ[i,:]=np.dot(rotation_matrix(axis,rad),v)
+      v=np.subtract(XYZ[i,:],p2)
+#      vrot=np.dot(rotation_matrix(axis,rad),v)
+      vrot=np.dot(rotation_matrix2(axis,rad),v)
+      XYZ[i,:]=np.add(vrot,p2)
     return
 
 def rotation_matrix(axis, theta):
@@ -130,14 +141,114 @@ def rotation_matrix(axis, theta):
     """
     axis = np.asarray(axis)
     theta = np.asarray(theta)
-    axis = axis/sqrt(np.dot(axis, axis))
-    a = cos(theta/2.0)
-    b, c, d = -axis*sin(theta/2.0)
+    axis = axis/np.sqrt(np.dot(axis, axis))
+    a = np.cos(theta/2.0)
+    b, c, d = -axis*np.sin(theta/2.0)
     aa, bb, cc, dd = a*a, b*b, c*c, d*d
     bc, ad, ac, ab, bd, cd = b*c, a*d, a*c, a*b, b*d, c*d
     return np.array([[aa+bb-cc-dd, 2*(bc+ad), 2*(bd-ac)],
                      [2*(bc-ad), aa+cc-bb-dd, 2*(cd+ab)],
                      [2*(bd+ac), 2*(cd-ab), aa+dd-bb-cc]])
+
+
+def rotation_matrix2(axis,theta):
+    """
+    from Rafal, a bit different (signs)
+    """
+    axis = axis/np.sqrt(np.dot(axis,axis))
+    a = np.cos(theta/2.0)
+    b,c,d = -axis*np.sin(theta/2)
+    return np.array([[a*a+b*b-c*c-d*d, 2*(b*c-a*d), 2*(b*d+a*c)],
+                     [2*(b*c+a*d), a*a+c*c-b*b-d*d, 2*(c*d-a*b)],
+                     [2*(b*d-a*c), 2*(c*d+a*b), a*a+d*d-b*b-c*c]])
+
+
+
+def RotMatArb(axis,theta,point,vec):
+    a=point[0]
+    b=point[1]
+    c=point[2]
+    axis = axis/np.sqrt(np.dot(axis,axis))
+    u=axis[0]
+    v=axis[1]
+    w=axis[2]
+    cosT=cos(theta)
+    oneMinusCosT=1.0-cosT
+    sinT=sin(theta)
+    v2,w2,u2 = v*v,w*w,u*u
+    #matrix element wise
+    m11 = u2 + (v2 + w2) * cosT;
+    m12 = u*v * oneMinusCosT - w*sinT
+    m13 = u*w * oneMinusCosT + v*sinT
+    m14 = (a*(v2 + w2) - u*(b*v + c*w))*oneMinusCosT + (b*w - c*v)*sinT
+
+    m21 = u*v * oneMinusCosT + w*sinT
+    m22 = v2 + (u2 + w2) * cosT
+    m23 = v*w * oneMinusCosT - u*sinT
+    m24 = (b*(u2 + w2) - v*(a*u + c*w))*oneMinusCosT  + (c*u - a*w)*sinT
+
+    m31 = u*w * oneMinusCosT - v*sinT
+    m32 = v*w * oneMinusCosT + u*sinT
+    m33 = w2 + (u2 + v2) * cosT
+    m34 = (c*(u2 + v2) - w*(a*u + b*v))*oneMinusCosT  + (a*v - b*u)*sinT
+#   temp:
+#    x=vec[0]
+#    y=vec[1]
+#    z=vec[2]
+#    rx = m11*x + m12*y + m13*z + m14;
+#    ry = m21*x + m22*y + m23*z + m24;
+#    rz = m31*x + m32*y + m33*z + m34;
+#    print 'rvec',rx,ry,rz
+#    return np.array([rx,ry,rz])
+    return np.array([[m11,m12,m13,m14],
+                    [m21,m22,m23,m24],
+                    [m31,m32,m33,m34],
+                    [0,0,0,1]])
+
+#    return np.array([[m11,m21,m31],
+#                     [m12,m22,m32],
+#                     [m13,m23,m33],
+#                     [m14,m24,m34]])
+
+
+
+
+def RotVecArb(axis,theta,point,vec):
+    """
+     rotation around arbitrary axis, following http://inside.mines.edu/fs_home/gmurray/ArbitraryAxisRotation/
+    """
+    print 'abc',point
+    print 'axis',axis
+    print 'vec',vec
+    a=point[0]
+    b=point[1]
+    c=point[2]
+    axis = axis/np.sqrt(np.dot(axis,axis))
+    u=axis[0]
+    v=axis[1]
+    w=axis[2]
+    x=vec[0]
+    y=vec[1]
+    z=vec[2]
+    cosT=cos(theta)
+    oneMinusCosT=1.0-cosT
+    sinT=sin(theta)
+    v2,w2,u2 = v*v,w*w,u*u
+    bv,cw,ux,vy,wz,cv,bw,wy,vz = b*v,c*w,u*x,v*y,w*z,c*v,b*w,w*y,v*z
+    au,cu,aw,wx,uz = a*u,c*u,a*w,w*x,u*z
+    bu,av,vx,uy = b*u,a*v,v*x,u*y
+#    rx=(a*(v2+w2)-u*(bv+cw-ux-vy-wz))*oneMinusCosT+x*cosT+(-cv+bw-wy+vz)*sinT
+#    ry=(b*(u2+w2)-v*(au+cw-ux-vy-wz))*oneMinusCosT+y*cosT+(cu-aw+wx-uz)*sinT
+#    rz=(c*(u2+v2)-w*(au+bv-ux-vy-wz))*oneMinusCosT+z*cosT+(-bu+av-vx+uy)*sinT
+#    print 'rvec',rx,ry,rz
+    rx = (a*(v2 + w2) - u*(b*v + c*w - u*x - v*y - w*z)) * oneMinusCosT+ x*cosT + (-c*v + b*w - w*y + v*z)*sinT
+
+    ry = (b*(u2 + w2) - v*(a*u + c*w - u*x - v*y - w*z)) * oneMinusCosT + y*cosT+ (c*u - a*w + w*x - u*z)*sinT
+
+    rz = (c*(u2 + v2) - w*(a*u + b*v - u*x - v*y - w*z)) * oneMinusCosT + z*cosT + (-b*u + a*v - v*x + u*y)*sinT
+    print 'rvec',rx,ry,rz
+    return np.array([rx,ry,rz])
+
 
 
 # return list of atoms connected to atom a
@@ -154,8 +265,8 @@ def c_dist(di,dj): ##calculate distance between 2 lines of coords
 def bond_mat(nat,elem,XYZ):
     cov={'h': 0.6430, 'he': 0.6430,'li': 2.4570,'be': 1.9090,'b': 1.5870, 'c':1.4360,'n': 1.3090,\
        'o': 1.0960, 'f': 1.1200, 'ne': 0.9450, 'na': 2.9860,'mg': 2.6460,'al':2.4000,'si': 2.1920,\
-       'p': 2.0600,'s': 1.8900,'cl': 1.7950,'ar': 1.7010,'k': 3.8360,'ca:' :3.2880,'sc':2.7210}
-#       2.4940,2.3050,2.2300,2.2110,2.2110,2.1920,2.1730,
+       'p': 2.0600,'s': 1.8900,'cl': 1.7950,'ar': 1.7010,'k': 3.8360,'ca:' :3.2880,'sc':2.7210,\
+       'ti': 2.4940, 'v': 2.3050, 'cr': 2.2300, 'mn': 2.2110,'fe': 2.2110,'co': 2.1920,'ni': 2.1730}
 #       2.2110,2.3620,2.3810,2.3050,2.2680,2.1920,2.1540,
 #       2.1160,4.0820,3.6090,3.0610,2.7400,2.5320,2.4570,
 #       2.4000,2.3620,2.3620,2.4190,2.5320,2.7970,2.7210,
@@ -176,6 +287,7 @@ def bond_mat(nat,elem,XYZ):
               if abs(dist-check) <= 0.5:
                    bonds.append((i,j))
     return bonds
+
 
 # --------------------------------------------------------------
 
@@ -259,8 +371,8 @@ for f in range(0,nr):
   if ifrag[f] == 1: 
     atlist=(frags[f])
     print 'rotating:',atlist
-    #rotmol(x1,x2,10,atlist,XYZ)
-    rotmolMAT(x1,x2,10,atlist,XYZ)
+    rotmol(x1,x2,20,atlist,XYZ)
+    #rotmolMAT(x1,x2,10,atlist,XYZ)
 
                                                  
 #sys.exit("debug end")
